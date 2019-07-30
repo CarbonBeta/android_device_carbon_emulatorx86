6e8ceea21e3749e9a0f7f633b44250d116ee8069
Joe Maples <joe@maples.dev>
2019-07-30 12:59:47 +0000

    emulator: Add emulatorimage build target
    Signed-off-by: Joe Maples <joe@maples.dev>

--- carbon_emulatorx86_64.mk
+++ carbon_emulatorx86_64.mk
@@ -12,3 +12,11 @@ PRODUCT_DEVICE := emulatorx86_64
 PRODUCT_BRAND := CarbonROM
 PRODUCT_MODEL := CarbonROM on x86_64 Emulator
 
+# Define emulator image creation
+emulatorimage: droid
+	cp -r $(LOCAL_PATH)/skins $(TARGET_PRODUCT_OUT)
+	cp -r $(LOCAL_PATH)/start_emulator_image.sh $(TARGET_PRODUCT_OUT)
+	cp -r $(LOCAL_PATH)/advancedFeatures.ini $(TARGET_PRODUCT_OUT)
+	rm /tmp/carbon_emulator.zip
+	cd $(TARGET_PRODUCT_OUT)
+	zip -r /tmp/carbon_emulator.zip skins system-qemu.img system/build.prop cache.img userdata.img start_emulator_image.sh advancedFetures.ini encryptionkey.img kernel-ranchu-64 ramdisk.img
